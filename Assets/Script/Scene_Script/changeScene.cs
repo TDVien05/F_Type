bcfd1ffcbaadba00b7afe3c70a5f77ad09e6b664
NgoMinhHieu <101505594+NgoHieu-coder@users.noreply.github.com>
2024-11-27 23:26:58 +0700

    HieuAddGameOverUI

--- Assets/Script/Scene_Script/changeScene.cs
+++ Assets/Script/Scene_Script/changeScene.cs
@@ -10,4 +10,39 @@ public class changeScene : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void MenuPage()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void PlayPage()
+    {
+        SceneManager.LoadScene("ModePlay");
+    }
+
+    public void Model30()
+    {
+        SceneManager.LoadScene("30s");
+    }
+
+    public void Model60()
+    {
+        SceneManager.LoadScene("60s");
+    }
+
+    public void ModelUnlimit()
+    {
+        SceneManager.LoadScene("Unlimitted");
+    }
+
+    public void Cancel()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
